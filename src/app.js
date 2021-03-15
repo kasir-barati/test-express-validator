5f8f9a5a4462162aa344f722da59e0506d0bd80b
nodejsdeveloperskh <node.js.developers.kh@gmail.com>
2021-03-15 12:00:42 +0330

    cleanup (main): prettier convert double quote to single qoute

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
+const express = require('express');
 
-const cors = require("./middleware/cors");
-const { appHost, appPort } = require("./config");
+const cors = require('./middleware/cors');
+const { appHost, appPort } = require('./config');
 
 const app = express();
 
@@ -9,11 +9,13 @@ app.use(cors);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(require("./router/auth"));
-app.use(require("./router/user"));
-app.use(require("./middleware/404"));
-app.use(require("./middleware/error"));
-app.use(require("./middleware/500"));
+app.use(require('./router/auth'));
+app.use(require('./router/user'));
+app.use(require('./middleware/404'));
+app.use(require('./middleware/error'));
+app.use(require('./middleware/500'));
 
 // app.listen(appPort, appHost, () => console.log(`Server is up on ${appPort}`));
-app.listen(appPort, () => console.log(`Server is up on ${appPort}`));
+app.listen(appPort, () =>
+  console.log(`Server is up on ${appPort}0`),
+);
